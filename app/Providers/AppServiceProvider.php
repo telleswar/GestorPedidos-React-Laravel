d4571877f94b4b0758660787a9600aeb52bc4588
telleswar <silva12341@gmail.com>
2024-03-13 13:59:38 -0300

    hh

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,8 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(config('app.env') === 'production') {
-            \URL::forceScheme('https');
-        }
+        URL::forceScheme('https');
     }
 }
