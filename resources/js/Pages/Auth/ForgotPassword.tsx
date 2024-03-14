b93945102b4a3aeccbe6b7eac18af727d8ea3495
telleswar <silva12341@gmail.com>
2024-03-14 12:32:36 -0300

    Estilização

--- resources/js/Pages/Auth/ForgotPassword.tsx
+++ resources/js/Pages/Auth/ForgotPassword.tsx
@@ -33,7 +33,7 @@ export default function ForgotPassword({ status }: { status?: string }) {
                     type="email"
                     name="email"
                     value={data.email}
-                    className="mt-1 block w-full"
+                    className="block w-full"
                     isFocused={true}
                     onChange={(e) => setData('email', e.target.value)}
                 />
